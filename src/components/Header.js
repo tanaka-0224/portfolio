d092abdd7c87e6db5f03d0497f30ec4371e2759b
tanaka-0224 <r.iku0224@icloud.com>
2024-03-04 23:43:49 +0900

    routingまで

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <h3>ポートフォリオ</h3>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">ホーム</Link>
+            <a href="#"></a>
+          </li>
+          <li>
+            <Link to="/blog">ブログ</Link>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#">SNS</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
